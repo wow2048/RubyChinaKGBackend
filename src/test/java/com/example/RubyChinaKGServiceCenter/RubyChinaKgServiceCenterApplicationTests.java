f3965bda1ddadb7c4fc970f8ecbb2b69677aef82
hefusen <3580476060@qq.com>
2023-11-01 23:11:35 +0800

    项目初始化

---
+++ src/test/java/com/example/RubyChinaKGServiceCenter/RubyChinaKgServiceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+/*package com.example.RubyChinaKGServiceCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubyChinaKgServiceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
